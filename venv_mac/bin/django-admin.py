a2927ea281938c4b007a016bafb1bf0879bc2167
Desmond Tan <max@Desmonds-MBP.mazx.local>
2020-06-14 17:04:54 +0800

    added venv for mac

---
+++ venv_mac/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/max/Downloads/botnet-enumeration-network-master/venv_mac/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
