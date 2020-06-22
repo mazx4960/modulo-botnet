b84545cc6cb4de84fe2353f18706b910e55e28fb
Desmond <desmondtan4561@gmail.com>
2020-06-22 14:07:32 +0800

    added templates and static directories

--- server_django/server_django/settings.py
+++ server_django/server_django/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'server_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'webui/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "webui/static"),
+]
