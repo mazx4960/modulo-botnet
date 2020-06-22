b4f15635ee97b4b59ad938ea167f9a51b43ba272
Desmond <desmondtan4561@gmail.com>
2020-06-22 22:05:29 +0800

    added tests for models

--- server_django/api/models.py
+++ server_django/api/models.py
@@ -2,22 +2,32 @@ from django.db import models
 from datetime import datetime
 
 
-class Agents(models.Model):
+class Agent(models.Model):
     id = models.AutoField(primary_key=True)
     display_name = models.CharField(max_length=100)
-    last_online = models.DateTimeField()
+    last_online = models.DateTimeField(default=datetime.now)
     remote_ip = models.CharField(max_length=100)
     operating_system = models.CharField(max_length=100)
     computer_name = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
 
+    def push_cmd(self, cmdline):
+        command = Command()
+        command.agent = self
+        command.cmdline = cmdline
+        command.timestamp = datetime.now()
+        command.save()
+
+    def is_online(self):
+        return (datetime.now() - self.last_online).seconds < 30
+
     def __str__(self):
         return 'Agent <{}> ({})'.format(self.display_name, self.remote_ip)
 
 
-class Commands(models.Model):
+class Command(models.Model):
     id = models.AutoField(primary_key=True)
-    agent = models.ForeignKey(Agents, on_delete=models.CASCADE)
+    agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     cmdline = models.CharField(max_length=255)
     timestamp = models.DateTimeField(default=datetime.now)
 
@@ -25,11 +35,11 @@ class Commands(models.Model):
         return self.cmdline
 
 
-class Users(models.Model):
+class User(models.Model):
     id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=200)
-    last_login = models.DateTimeField()
+    last_login = models.DateTimeField(default=datetime.now)
     last_login_ip = models.CharField(max_length=100)
 
     def __str__(self):
--- server_django/api/tests.py
+++ server_django/api/tests.py
@@ -1,3 +1,27 @@
+import datetime
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Agent, Command, User
+
+
+class AgentModelTest(TestCase):
+    def test_is_online_within_30_seconds(self):
+        """
+        is_online() returns true if the last login was within 30 seconds
+        """
+        time_10_secs_ago = datetime.datetime.now() - datetime.timedelta(seconds=10)
+        agent_online_10_secs_ago = Agent(last_online=time_10_secs_ago)
+        self.assertIs(agent_online_10_secs_ago.is_online(), True)
+
+    def test_is_online_more_than_30_seconds(self):
+        """
+        is_online() returns False if the last login was more than 30 seconds ago
+        """
+        time_60_secs_ago = datetime.datetime.now() - datetime.timedelta(seconds=60)
+        agent_online_60_secs_ago = Agent(last_online=time_60_secs_ago)
+        self.assertIs(agent_online_60_secs_ago.is_online(), False)
+
+
+class ApiTest(TestCase):
+    pass
