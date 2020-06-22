0fbe1dc7147a6dca6a6d488ba101922feb03c39a
Desmond <desmondtan4561@gmail.com>
2020-06-22 13:42:25 +0800

    Fixed django configurations and added api support

--- server_django/cnc/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CncConfig(AppConfig):
-    name = 'cnc'
---
+++ venv_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\desmo\desktop\botnet-enumeration-network\venv_windows\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
