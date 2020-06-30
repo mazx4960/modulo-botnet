3af3fd63cf708f6d93edc194ee98c153f95a4b91
xiuqiho <xiuqiho@gmail.com>
2020-06-30 19:55:29 +0800

    Removed Secret Key for security purposes
    Refer to telegram to add Secret Key into settings.py of the project

--- C2/C2/settings.py
+++ C2/C2/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'zf^b@k5@==^axfpdm05im_)x%8k+*v%k)%o-aw(w54gsj(p!wc'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
